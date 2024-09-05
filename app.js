3de9ced0084f9138ef99ea98754b95e607d4094d
Matheus Miranda <matheuslmiranda601@gmail.com>
2024-09-05 10:54:22 -0300

    atualizando projeto

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log (dados);
+function pesquisar() {
+    // Seleciona o elemento HTML com o ID "resultados-pesquisa"
+    // onde os resultados serão exibidos.
+    let section = document.getElementById("resultados-pesquisa");
+
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa.
+    let resultados = "";
+
+    // Itera sobre cada dado na lista de dados.
+    // Para cada dado, cria um elemento HTML com as informações do dado
+    // e adiciona-o à string 'resultados'.
+    for (let dado of dados) {
+        resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">Onde Comprar</a>
+            </div>
+        `;
+    }
+
+    // Atribui o conteúdo da string 'resultados' ao elemento 'section',
+    // atualizando a seção com os resultados da pesquisa.
+    section.innerHTML = resultados;
+}
--- dados.js
+++ dados.js
@@ -1,23 +1,17 @@
 let dados = [
-    "WLMouse Beast X",
-    "Razer Viper V3 Pro",
-    "Ninjutsu Sora 4k"
+    {
+        titulo: "WLMouse Beast X",
+        descricao: " O WLmouse Beast X é um mouse gamer de alta performance, projetado para oferecer uma experiência de jogo excepcional. Sua construção em liga de magnésio o torna extremamente leve, reduzindo a fadiga durante longas sessões de jogo ",
+        link: " https://doctormouse.com.br/produtos/wlmouse-beast-max-8k/ "
+    },
+    {
+        titulo: "Razer Viper V3 Pro",
+        descricao: "O Razer Viper V3 Pro é um mouse gamer ultraleve com switches ópticos de terceira geração Razer, oferecendo uma resposta rápida e precisa. Seu design ergonômico e sensor óptico Focus Pro 30K garantem um desempenho excepcional em jogos competitivos.",
+        link: "https://www.razer.com/gaming-mice/razer-viper-v3-pro"
+    },
+    {
+        titulo: "Ninjutsu Sora 4K",
+        descricao: "O Ninjutsu Sora 4K é um mouse gamer sem fio de alta precisão, projetado para jogadores que buscam leveza e conforto. Seu sensor óptico PixArt PAW3399 oferece um rastreamento preciso e rápido, ideal para jogos FPS e MOBA.",
+        link: "https://www.ninjutsu.com.br/produtos/mouse-gamer-ninjutsu-sora-4k"
+    } 
 ];
-
-let wlmouse = {
-    titulo: "WLMouse Beast X",
-    descricao: " O WLmouse Beast X é um mouse gamer de alta performance, projetado para oferecer uma experiência de jogo excepcional. Sua construção em liga de magnésio o torna extremamente leve, reduzindo a fadiga durante longas sessões de jogo ",
-    link: " https://doctormouse.com.br/produtos/wlmouse-beast-max-8k/ "
-}
-
-let razerviperv3pro = {
-    titulo: "Razer Viper V3 Pro",
-    descricao: "O Razer Viper V3 Pro é um mouse gamer ultraleve com switches ópticos de terceira geração Razer, oferecendo uma resposta rápida e precisa. Seu design ergonômico e sensor óptico Focus Pro 30K garantem um desempenho excepcional em jogos competitivos.",
-    link: "https://www.razer.com/gaming-mice/razer-viper-v3-pro"
-};
-
-let ninjutsusora4k = {
-    titulo: "Ninjutsu Sora 4K",
-    descricao: "O Ninjutsu Sora 4K é um mouse gamer sem fio de alta precisão, projetado para jogadores que buscam leveza e conforto. Seu sensor óptico PixArt PAW3399 oferece um rastreamento preciso e rápido, ideal para jogos FPS e MOBA.",
-    link: "https://www.ninjutsu.com.br/produtos/mouse-gamer-ninjutsu-sora-4k"
-};
