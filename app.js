7478941a163221f27b22c89fbcf9f9432d62b36c
Matheus Miranda <matheuslmiranda601@gmail.com>
2024-09-06 11:09:55 -0300

    projeto atualizado

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 function pesquisar() {
     // Seleciona o elemento HTML com o ID "resultados-pesquisa"
     // onde os resultados serão exibidos.
-    let section = document.getElementById("resultados-pesquisa");
+    let section = document.getElementById ("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById ("campo-pesquisa").value
+
+    // se campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome do mouse ou característica dele</p>"
+        return 
+    }
+    campoPesquisa = campoPesquisa.toLowerCase()
 
     // Inicializa uma string vazia para armazenar os resultados da pesquisa.
     let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
 
     // Itera sobre cada dado na lista de dados.
     // Para cada dado, cria um elemento HTML com as informações do dado
     // e adiciona-o à string 'resultados'.
     for (let dado of dados) {
-        resultados += `
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        // se titulo includes campoPesquisa
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // cria um novo elemento
+            resultados += `
             <div class="item-resultado">
                 <h2>
                     <a href="#" target="_blank">${dado.titulo}</a>
@@ -19,6 +37,12 @@ function pesquisar() {
                 <a href=${dado.link} target="_blank">Onde Comprar</a>
             </div>
         `;
+        }
+        
+    }
+
+    if(!resultados) {
+        resultados = "<p>Nada foi encontrdo</p>"
     }
 
     // Atribui o conteúdo da string 'resultados' ao elemento 'section',
--- dados.js
+++ dados.js
@@ -2,16 +2,19 @@ let dados = [
     {
         titulo: "WLMouse Beast X",
         descricao: " O WLmouse Beast X é um mouse gamer de alta performance, projetado para oferecer uma experiência de jogo excepcional. Sua construção em liga de magnésio o torna extremamente leve, reduzindo a fadiga durante longas sessões de jogo ",
-        link: " https://doctormouse.com.br/produtos/wlmouse-beast-max-8k/ "
+        link: " https://doctormouse.com.br/produtos/wlmouse-beast-max-8k/ ",
+        tags:"besta vazado dongle 8k"
     },
     {
         titulo: "Razer Viper V3 Pro",
         descricao: "O Razer Viper V3 Pro é um mouse gamer ultraleve com switches ópticos de terceira geração Razer, oferecendo uma resposta rápida e precisa. Seu design ergonômico e sensor óptico Focus Pro 30K garantem um desempenho excepcional em jogos competitivos.",
-        link: "https://www.razer.com/gaming-mice/razer-viper-v3-pro"
+        link: "https://www.razer.com/gaming-mice/razer-viper-v3-pro",
+        tags:"ergonomico 30k leve"
     },
     {
         titulo: "Ninjutsu Sora 4K",
         descricao: "O Ninjutsu Sora 4K é um mouse gamer sem fio de alta precisão, projetado para jogadores que buscam leveza e conforto. Seu sensor óptico PixArt PAW3399 oferece um rastreamento preciso e rápido, ideal para jogos FPS e MOBA.",
-        link: "https://www.ninjutsu.com.br/produtos/mouse-gamer-ninjutsu-sora-4k"
+        link: "https://www.ninjutsu.com.br/produtos/mouse-gamer-ninjutsu-sora-4k",
+        tags:"moba fps 3399"
     } 
 ];
